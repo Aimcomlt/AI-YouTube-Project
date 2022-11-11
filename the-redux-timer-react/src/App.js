a99624ab587fca24e52fa6bb443fe9acb1f05832
Aime Comeault <67168818+Aimcomlt@users.noreply.github.com>
2022-11-11 14:13:39 -0700

    added Timer reducer

--- the-redux-timer-react/src/App.js
+++ the-redux-timer-react/src/App.js
@@ -1,11 +1,17 @@
-
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import {timer} from './Timer';
+import reducers from './reducers';
 import './App.css';
+
+const store = createStore(reducers);
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Provider store={store}>
+      <h1>TIMER</h1>
+      
+    </Provider>
   );
 }
 
